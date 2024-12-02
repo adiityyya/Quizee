ba314875768c3a63ad92503aa9d2f5b640d350c1
adiityyya <cocaslps@gmail.com>
2024-12-03 01:55:34 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Quiz from "./components/quiz/Quiz";
-import Login from "./pages/login/login";
+import Login from "./pages/login/Login";
+
 import Signup from "./pages/signup/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
