e7a2d621b433397ec07edf2677e054ab053508d5
adiityyya <cocaslps@gmail.com>
2024-12-03 01:44:05 +0530

    removed dist

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,8 @@ app.use("/api/auth",authRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
-
 
 app.listen(port,()=>{
     connectToMongoDB();
