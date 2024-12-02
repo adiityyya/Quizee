f7b0470d7ea21b2db3313e564750caabcb17658a
adiityyya <cocaslps@gmail.com>
2024-12-03 02:24:33 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import connectToMongoDB from './db/connectToMongoDb.js';
-import quizRoutes from './routes/quizRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import connectToMongoDB from "./db/connectToMongoDb.js";
+import quizRoutes from "./routes/quizRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 import path from "path";
 
 const port = 5000;
 const app = express();
-
 
 const __dirname = path.resolve();
 //MIDDLEWARES
@@ -19,16 +18,24 @@ app.use(cookieParser());
 dotenv.config();
 
 //ROUTES
-app.use("/api/quiz",quizRoutes);
-app.use("/api/auth",authRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
-app.listen(port,()=>{
-    connectToMongoDB();
+app.listen(port, () => {
+    connectToMongoDB()
+      .then(() => {
+        console.log('Connected to MongoDB successfully');
+      })
+      .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+      });
+  
     console.log(`Server Running on port ${port}`);
-})
+  });
+  
