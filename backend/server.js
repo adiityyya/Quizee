d0f5a64f0a877a01682dc20161ba6870772e35db
adiityyya <cocaslps@gmail.com>
2024-12-07 00:24:07 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,29 @@ import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 
-const port = 5000;
-const app = express();
-
-const __dirname = path.resolve();
-
-//MIDDLEWARES
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
 dotenv.config();
 
-//ROUTES
+const port = process.env.PORT || 5000;
+const app = express();
+
+// MIDDLEWARES
+app.use(cors({
+  origin: "https://your-frontend-domain.vercel.app",
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+// ROUTES
 app.use("/api/quiz", quizRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-  // console.log("hello ji");
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.get("/", (req,res)=>{
+  res.send("HI!")
 });
-
+// SERVER
 app.listen(port, () => {
   connectToMongoDB();
-  console.log(`Server Running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
+// Load environment variables from .env
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-		port: 5173,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
-})
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_REACT_APP_BACKEND_BASEURL, // Use process.env
+        // changeOrigin: true,
+        // secure: false,
+        // target: "http://localhost:5000",
+        // target: process.env.VITE_REACT_APP_BACKEND_BASEURL,
+        // target: import.meta.env.VITE_REACT_APP_BACKEND_BASEURL,
+      },
+    },
+  },
+});
