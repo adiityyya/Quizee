126b83d9ee08e833ae4297bcc79dbca79c2e4548
adiityyya <cocaslps@gmail.com>
2024-12-07 21:41:23 +0530

    changed folder structure

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,20 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 
+// dotenv.config({ path: "./.env" });
 dotenv.config();
-const __dirname = path.resolve();
+
+// const __dirname = path.resolve();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MIDDLEWARES
 app.use(cors({
   origin: process.env.frontend_url,
+  methods: ["POST","GET"],
   credentials: true
 }));
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,11 +28,17 @@ app.use(cookieParser());
 app.use("/api/quiz", quizRoutes);
 app.use("/api/auth", authRoutes);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   // console.log("hello ji");
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.send("HI");
 });
+
+// app.get("*", (req, res) => {
+//   // console.log("hello ji");
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 // SERVER
+// console.log("url: ", process.env.MONGO_DB_URI)
 app.listen(port, () => {
   connectToMongoDB();
   console.log(`Server running on port ${port}`);
